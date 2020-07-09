6dfe4d8113c3645975c1eec5a1fde3e6c05011da
sequoyah <sequoyah@localhost>
2020-07-08 20:29:53 -0400

    test

--- true_sys.m
+++ true_sys.m
@@ -5,7 +5,7 @@ function dydt = true_sys(y,T,A,L)
     m = 60000;  % mass is 60,000 kg
     g = 9.8;    % accel due to grav is 9.8 ms^(-2)
     
-    dydt = zeros(3,1);
+    dydt = zeros(3,1); 
     dydt(1) = (1/m)*(T*cosd(A) - (2.7 + 3.08*(1.25 + 4.2*A)^2)*y(1)^2 - m*g*sind(y(2)));
     dydt(2) = (1/(m*y(1)))*(T*sind(A) + L*68.6*(1.25 + 4.2*A)*y(1)^2 - m*g*cosd(y(2)));
     dydt(3) = y(1)*sind(y(2));
