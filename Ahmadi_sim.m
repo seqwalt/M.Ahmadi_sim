4b3114a0ee238a80f09c81a6e0c40ce58d3430fd
Sequoyah Walters <sequoyah.n.walters@gmail.com>
2020-07-28 11:04:36 -0400

    Update Ahmadi_sim.m

--- Ahmadi_sim.m
+++ Ahmadi_sim.m
@@ -4,16 +4,16 @@
 % Updated 7/08/20 by Sequoyah Walters
 
 %% --- Parameter Panel --- %%
-T_max = 160000;     % max thrust (N)
-T = 0.2*T_max;      % Thrust
-T_no_u = 0;         % Thrust with no input
+Th_max = 160000;     % max thrust (N)
+Th = 0.2*Th_max;      % Thrust
+Th_no_u = 0;         % Thrust with no input
 A = -.9;            % Angle of attack (use -0.9 for engine failure)
 L = 1;              % Lift starts at 100%
-u1a = 0.3*T;        % 1 corresponds to thrust
+u1a = 0.3*Th;        % 1 corresponds to thrust
 u2a = 0.8*A;        % 2 corresponds to ang of attack
-u1b = T;            % b corresponds to final trajectory in the 3-traj approx
+u1b = Th;            % b corresponds to final trajectory in the 3-traj approx
 u2b = A;
-%si = [-0.07, 0, 0.07];    % side information
+%si = [0, -0.07, 0.07];    % side information
 si = 0;
 len_si = length(si);
 y0 = [95 2.2 120];    % initial values of velocity (m/s), flight path angle (deg), altitude (m)
@@ -30,7 +30,7 @@ ex_k = 3;             % Order of extrapolation
 %% --- Aircraft Flight and Failure --- %%
 
 % Solve until t_f (system failure) %
-[t,y] = ode45(@(t,y) true_sys(y,T,A,L), tspan(1:find(tspan==t_f)), y0); 
+[t,y] = ode45(@(t,y) true_sys(y,Th,A,L), tspan(1:find(tspan==t_f)), y0); 
 S = [t(1:end-1),y(1:end-1,:)]; % Store history of states in S (except last state)
 y_end = y(end,:);              % Last state will be used as initial cond. for next ode45
 
@@ -40,20 +40,20 @@ if strcmp(failure,'wing') == 1
     L = 0;
 elseif strcmp(failure,'engine') == 1
     % Engine failure: 20% T_max -> 80% T_max (interpreted as 60% max thrust when no input is given)
-    T_no_u = 0.6*T_max;
+    Th_no_u = 0.6*Th_max;
 end
-[t,y] = ode45(@(t,y) true_sys(y,T_no_u,0,L), tspan(find(tspan==t_f):find(tspan==t_f+dur)), y_end);
+[t,y] = ode45(@(t,y) true_sys(y,Th_no_u,0,L), tspan(find(tspan==t_f):find(tspan==t_f+dur)), y_end);
 S_no_u = [t,y];
-[t,y] = ode45(@(t,y) true_sys(y,u1a + T_no_u,u2a,L), tspan(find(tspan==t_f):find(tspan==t_f+dur)), y_end); 
+[t,y] = ode45(@(t,y) true_sys(y,u1a + Th_no_u,u2a,L), tspan(find(tspan==t_f):find(tspan==t_f+dur)), y_end); 
 S_a = [t,y];
 
 % --- True flight system --- %
-[t,y] = ode45(@(t,y) true_sys(y,u1b + T_no_u,u2b,L), tspan(find(tspan==t_f):find(tspan==t_f+dur)), y_end);
+[t,y] = ode45(@(t,y) true_sys(y,u1b + Th_no_u,u2b,L), tspan(find(tspan==t_f):find(tspan==t_f+dur)), y_end);
 S_true_pre = [t,y];
 S_true = [S;t,y];
 y_end_data = y(end,:);
     % The rest of the flight if there were no controller
-[t,y] = ode45(@(t,y) true_sys(y,u1b + T_no_u,u2b,L), tspan(find(tspan==t_f+dur):end), y_end_data);
+[t,y] = ode45(@(t,y) true_sys(y,u1b + Th_no_u,u2b,L), tspan(find(tspan==t_f+dur):end), y_end_data);
 S_true = [S_true;t,y];
 
 times = S_no_u(:,1);  % all times are the same for S_no_u, S_u1 and S_u2
@@ -81,35 +81,74 @@ for si1_ind = 1:len_si
         S_approx(si1_ind,si2_ind) = {[t,x]};
     end
 end
+
+%% --- Remove Negative Altitude Data --- %%
+for si1_ind = 1:len_si
+    for si2_ind = 1:len_si
+        S_approx{si1_ind,si2_ind} = above_zero_alt(S_approx{si1_ind,si2_ind},'approximate');
+    end
+end
+S_true   = above_zero_alt(S_true,  '       true');
+
 %% --- Controller Synthesis and Usage --- %%
 
 % Find B (Lyapunov Function)
 
 m = 60000;  % mass is 60,000 kg
-g = 9.8;    % accel due to grav is 9.8 ms^(-2)
+grav = 9.8;    % accel due to grav is 9.8 ms^(-2)
+v_safe = 86;
+M = 10;
+wait = 1; % wait 1 sec after approx is started to engage controller
+
+t_N   = t_f;                   % time t_N
+v_t_N = S_true(S_true==t_N,2); % velocity @ time t_N
+p_t_N = S_true(S_true==t_N,3); % flight path angle @ time t_N
+a_t_N = S_true(S_true==t_N,4); % altitude @ time t_N
+
+T   = t_f + dur + wait;    % time T
+v_T = S_true(S_true==T,2); % velocity @ time T
+p_T = S_true(S_true==T,3); % flight path angle @ time T
+a_T = S_true(S_true==T,4); % altitude @ time T
 
 echo on;
-syms v p a t; % vel, fpa, alt, time
-vars = [v; p; a];
-% Constructing the vector field dx/dt = f
-f = [(1/m)*(T*cosd(A) - (2.7 + 3.08*(1.25 + 4.2*A)^2)*v^2 - m*g*p);
-     (1/(m*v))*(T*sind(A) + L*68.6*(1.25 + 4.2*A)*v^2 - m*g*(1-(p^2)/2));
-     v*p];
+syms v p a t m1 m2 s c q2; % vel, fpa, alt, time, constant
+vars = [v; p; a; t; m1; m2; s; c; q2];
+% Constructing the vector field dx/dt = f + g*u
+
+f = [(-7.5125*v^2/m) - (m*grav*p*pi/180);
+     (85.75*v/m) - (grav/v);
+     (v*p*pi/180)];
+ 
+g = [1/m , -32.34*v^2/m;
+     0   , (pi*Th/(180*m*v)) + (288.12*v/m);
+     0   , 0];
+ 
 % =============================================
 % First, initialize the sum of squares program
 prog = sosprogram(vars);
 % =============================================
 % The Lyapunov function V(x):
-[prog,B] = sospolyvar(prog,[v^3; p^3; a^3; t^3],'wscoeff');
-[prog,W] = sospolyvar(prog,[v^3; p^3; a^3; t^3],'wscoeff');
+[prog,B(v,p,a,t)]  = sospolyvar(prog,[v^3; p^3; a^3; t^3],'wscoeff');
+[prog,W(v,p,a,t)]  = sospolyvar(prog,[v^3; p^3; a^3; t^3],'wscoeff');
 % =============================================
 % Next, define SOSP constraints
-% Constraint 1
-prog = sosineq(prog,B-(v^2+p^2+a^2));
-% Constraint 2
-expr = -(diff(B,t) + diff(B,v)*(f(1)-M) + diff(B,p)*(f(2)-M) + diff(B,a)*(f(3)-M) + W)*v;
-prog = sosineq(prog,expr);
+prog = sosineq(prog,W(v,p,a,t));
+prog = sosineq(prog,s);
+prog = sosineq(prog,m1);
+prog = sosineq(prog,m2);
+prog = sosineq(prog,q2);
+q1(v,p,a,t) = v^2 + p^2 + a^2 +t^2;
 
+expr1 = B(v_T,p_T,a_T,T) - B(v_t_N,p_t_N,a_t_N,t_N) + s*(v_safe-v) - c;
+expr2 = -(diff(B(v,p,a,t),t) + diff(B(v,p,a,t),v)*(f(1)-M) + diff(B(v,p,a,t),p)*(f(2)-M) + diff(B(v,p,a,t),a)*(f(3)-M) + W(v,p,a,t) + m1*(t-t_N)*(t-T))*v;
+expr3 = -(diff(B(v,p,a,t),t) + diff(B(v,p,a,t),v)*(f(1)+M) + diff(B(v,p,a,t),p)*(f(2)+M) + diff(B(v,p,a,t),a)*(f(3)+M) + W(v,p,a,t) + m2*(t-t_N)*(t-T))*v;
+expr4 = -q1(v,p,a,t)*mtimes([diff(B(v,p,a,t),v), diff(B(v,p,a,t),p), diff(B(v,p,a,t),a)],g) - q2;
+
+[prog,exprr4] = sospolymatrixvar(prog,expr4,[1,2]);
+prog = sosineq(prog,expr1);
+prog = sosineq(prog,expr2);
+prog = sosineq(prog,expr3);
+prog = sosineq(prog,expr4);
 % =============================================
 % And call solver
 solver_opt.solver = 'sedumi';
@@ -123,15 +162,7 @@ echo off;
 
 % Controller
 
-u = g'*dBdx*W/(dBdx'*g*g'*dBdx);
-
-%% --- Remove Negative Altitude Data --- %%
-for si1_ind = 1:len_si
-    for si2_ind = 1:len_si
-        S_approx{si1_ind,si2_ind} = above_zero_alt(S_approx{si1_ind,si2_ind},'approximate');
-    end
-end
-S_true   = above_zero_alt(S_true,  '       true');
+u = g'*dBdx*W/(dBdx'*(g*g')*dBdx);
 
 %% --- Plot --- %%
 figure
@@ -152,7 +183,7 @@ end
 
 plot3(S_true(1,2),S_true(1,3),S_true(1,4),'ko') % start
 plot3(S_true(S_true==t_f,2),S_true(S_true==t_f,3),S_true(S_true==t_f,4),'k*') % failure
-plot3(S_approx{1,1}(1,2),S_approx{1,1}(1,3),S_approx{1,1}(1,4),'k^') % data record stops
+plot3(S_approx{1,1}(1,2),S_approx{1,1}(1,3),S_approx{1,1}(1,4),'k^') % Start of approximation
 
 xlabel('Velocity (m/s)')
 ylabel('Flight path angle (deg)')
